6ca9f8251c18fe9dbdd9856e157163f99072bb92
Daniel L. Flath <dflath@slac.stanford.edu>
2014-10-16 17:50:25 +0000

    Add more fields to find_ioc output
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/package/trunk/ioc@19118 ececa60f-9f63-0410-97c7-ea05888eb260

--- find_ioc.py
+++ find_ioc.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
         sys.exit(1)
     iocs = utils.find_iocs(id=options.name)
     for ioc in iocs:
-        print "\tCONFIG:\t\t%s\n\tHOST:\t\t%s\n\tPORT:\t\t%s\n\tENABLED:\t%s" % \
-              (ioc[0], ioc[1].get('host'), ioc[1].get('port'), not ioc[1].get('disable'))
+        print "\tCONFIG:\t\t%s\n\tALIAS:\t\t%s\n\tDIR:\t\t%s\n\tCMD:\t\t%s\n\tHOST:\t\t%s\n\tPORT:\t\t%s\n\tENABLED:\t%s" % \
+              (ioc[0], ioc[1].get('alias'), ioc[1].get('dir'), ioc[1].get('cmd'), ioc[1].get('host'), ioc[1].get('port'), not ioc[1].get('disable'))
     sys.exit(0)
