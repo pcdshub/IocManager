f26c2cf09b7ebbb280dff89eb81607bd42cab699
Michael C. Browne <mcbrowne@slac.stanford.edu>
2015-05-13 20:27:35 +0000

    Erroneous check in of one-time csh fix.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/package/trunk/ioc@21477 ececa60f-9f63-0410-97c7-ea05888eb260

--- fixTelnet.py
+++ fixTelnet.py
@@ -3,8 +3,5 @@ import sys
 import utils
 
 if __name__ == '__main__':
-# port = sys.argv[1]
-# utils.fixTelnetShell('localhost', port)
-  host = sys.argv[1]
-  port = sys.argv[2]
-  utils.fixTelnetShell(host, port)
+  port = sys.argv[1]
+  utils.fixTelnetShell('localhost', port)
