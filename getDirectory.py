1000bededeea5851b399e50da867f20bd72886aa
Bruce Hill <bhill@slac.stanford.edu>
2020-05-28 11:18:12 -0700

    Improve error msg for NO_DIRECTORY

--- getDirectory.py
+++ getDirectory.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
   cfg  = sys.argv[2]
   result = utils.readConfig(cfg)
   if result == None:
-      print "NO_DIRECTORY"
+      print "NO_DIRECTORY configuration for %s!" % cfg
       sys.exit(-1)
   (mtime, config, hosts, vdict) = result
   for l in config:
